d173ddca55387ba9caf5c3513e2994a0bc0ac1c1
Puneet Singh <puneetsingh10091998@gmail.com>
2024-07-08 17:50:57 +0530

    Create copyArray.go

---
+++ copyArray.go
@@ -0,0 +1,17 @@
+package main;
+import "fmt";
+
+    type  copyArrayInterface interface{
+         copyArray(array []int)([]int);
+    }
+    func copyArray(array []int)([]int){
+        arrayCopy := make([]int, len(array))
+        for i:=0;i<len(array);i++{
+            arrayCopy[i]=array[i];
+        }
+        return arrayCopy
+    }
+    func main(){
+    array:=[]int{1,2,3,4,5};
+    fmt.Println("----",copyArray(array));
+}
