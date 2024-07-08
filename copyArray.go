a3f385cac6ea323864f2f3121527c4086896bef0
Puneet Singh <puneetsingh10091998@gmail.com>
2024-07-08 17:51:40 +0530

    Update copyArray.go

--- copyArray.go
+++ copyArray.go
@@ -5,6 +5,7 @@ import "fmt";
          copyArray(array []int)([]int);
     }
     func copyArray(array []int)([]int){
+        // use this make function when you dont know the size of array.
         arrayCopy := make([]int, len(array))
         for i:=0;i<len(array);i++{
             arrayCopy[i]=array[i];
